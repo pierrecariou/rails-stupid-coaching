29b04cf8b2f832bafe2dd13cd707483074f1299c
Pierre Cariou <pierrecariou@outlook.fr>
2019-02-11 12:47:20 +0100

    rails-stupid-coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
 end
